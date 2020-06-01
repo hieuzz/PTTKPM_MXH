a8bc09eac8ab5e74ab2395c404ebeb8105d5714f
nhudo267 <nhudo164@gmail.com>
2020-06-01 09:28:13 +0700

    update

--- server/models/post.js
+++ server/models/post.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
-const {ObjectId}= mongoose.Schema.Types
+const {ObjectId} = mongoose.Schema.Types
 const postSchema = new mongoose.Schema({
     title:{
-        type: String,
-        required: true
+        type:String,
+        required:true
     },
     body:{
-        type: String,
-        required: true
+        type:String,
+        required:true
     },
     photo:{
-        type: String,
-        required: true
+        type:String,
+        required:true
     },
     likes:[{type:ObjectId,ref:"User"}],
     comments:[{
@@ -19,8 +19,8 @@ const postSchema = new mongoose.Schema({
         postedBy:{type:ObjectId,ref:"User"}
     }],
     postedBy:{
-        type: ObjectId,
-        ref:"User"
+       type:ObjectId,
+       ref:"User"
     }
 })
 
